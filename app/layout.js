d646b8df005c10b22d996fc19cbd7c50738537ca
Patrick Shannon <peshannon104@gmail.com>
2024-01-15 17:14:04 -0400

    fixed build

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import { Inter } from 'next/font/google'
-import { SpeedInsights } from '@vercel/speed-insights/*'
+import { Inter } from 'next/font/google';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css'
 const inter = Inter({
   subsets: ['latin'],
